d5fe55e0d801ec4ddec29a0d01ef897397983dd0
AndreiLhu <leahu.andreilucian@gmail.com>
2023-04-18 15:52:01 +0200

    Changing dotenv config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const Dotenv = require('dotenv-webpack');
 
-module.exports = nextConfig
+module.exports = {
+  webpack: (config) => {
+    config.plugins.push(new Dotenv());
+    return config;
+  },
+};
