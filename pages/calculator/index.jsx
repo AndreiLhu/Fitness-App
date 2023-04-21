b47ab0a469e20f44484cec014a28dac80e8c728b
AndreiLhu <leahu.andreilucian@gmail.com>
2023-04-21 11:35:19 +0200

    Fitst sketch to home page

--- pages/calculator/index.jsx
+++ pages/calculator/index.jsx
@@ -7,7 +7,7 @@ const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100vh;
+  height: 80vh;
 
   form {
     background-color: #fff;
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,64 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
-
 import Link from 'next/link';
-// import MobileNavbar from '@/components/MobileNavbar';
+import styled from 'styled-components';
 
 const inter = Inter({ subsets: ['latin'] });
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 70vh;
+  background-color: #fff;
+  border-radius: 8px;
+  padding: 32px;
+  box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.1);
+
+  div[id='iconContainer'] {
+    display: flex;
+    justify-content: space-between;
+    background-color: #fff;
+    border-radius: 8px;
+    padding: 32px;
+    box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.1);
+  }
+  a {
+    margin: 0 16px;
+  }
+  h1 {
+    padding-bottom: 8em;
+  }
+`;
 
 export default function Home() {
   return (
     <>
-      {/* <MobileNavbar /> */}
-      <h1>This is the index.js page</h1>
+      <Container>
+        <h1>Fitness App!</h1>
+        <div id="iconContainer">
+          <Link className="white" href={'/exercises'}>
+            <Image
+              src="/exercises.svg"
+              alt="exercises"
+              width="100"
+              height="100"
+            />
+          </Link>
+          <Link className="white" href={'/recipe'}>
+            <Image src="/recipe.svg" alt="recipes" width="100" height="100" />
+          </Link>
+          <Link className="white" href={'/calculator'}>
+            <Image
+              src="/calculator.svg"
+              alt="calculator"
+              width="100"
+              height="100"
+            />
+          </Link>
+        </div>
+      </Container>
     </>
   );
 }
