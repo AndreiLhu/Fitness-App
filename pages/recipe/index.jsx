82f24abf04bc57e1e1e9ef51fc6731ab6c5f07b1
AndreiLhu <leahu.andreilucian@gmail.com>
2023-04-26 12:53:41 +0200

    API fetch. first sketch working

--- pages/recipe/index.jsx
+++ pages/recipe/index.jsx
@@ -1,7 +1,52 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
 
 const Recipe = () => {
-  return <div>index</div>;
+  const [recipesData, setRecipesData] = useState(null);
+  const [selectedRecipe, setSelectedRecipe] = useState('italian wedding soup');
+
+  const axios = require('axios');
+  const query = 'italian wedding soup';
+  useEffect(() => {
+    axios
+      .get(`https://api.api-ninjas.com/v1/recipe?query=${query}`, {
+        headers: {
+          'X-Api-Key': process.env.API_KEY,
+        },
+      })
+      .then((response) => {
+        const dataWithIds = response.data.map((item) => {
+          return {
+            ...item,
+            id: uuidv4(),
+          };
+        });
+
+        setRecipesData(dataWithIds);
+      })
+      .catch((error) => {
+        console.error('Request failed:', error);
+      });
+  }, [selectedRecipe]);
+
+  if (!recipesData) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div>
+      <form>
+        {recipesData.map((recipes, index) => (
+          <ul key={index}>
+            <h2>{recipes.title}</h2>
+            <li>{recipes.ingredients}</li>
+            <li>{recipes.servings}</li>
+            <p>{recipes.instructions}</p>
+          </ul>
+        ))}
+      </form>
+    </div>
+  );
 };
 
 export default Recipe;
